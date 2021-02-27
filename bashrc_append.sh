ef30d9dbd3fa197e920f6634d29cdbd502f6c54b
wfoschiera <wfoschiera@gmail.com>
2021-02-27 17:12:45 -0400

    corrigido EOL error

--- bashrc_append.sh
+++ bashrc_append.sh
@@ -1,15 +1,16 @@
 sudo tee -a $HOME/.bashrc <<'EOF'
-fi
+
 export PIPENV_VENV_IN_PROJECT=1
 export PATH="$HOME/.pyenv/bin:$PATH"
 
-alias mng='python $VIRTUAL_ENV/../manage.py'
-
-eval "$(pyenv init -)"
-
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
--e if command -v pyenv 1>/dev/null 2>&1; then
- eval "$(pyenv init -)"
+
+# seta ambiente para verificar se existe venv criado ou nao
+if command -v pyenv 1>/dev/null 2>&1; then
+    eval "$(pyenv init -)"
 fi
+eval "$(pyenv virtualenv-init -)"
+
+alias mng="python $VIRTUAL_ENV/../manage.py"
 EOF
